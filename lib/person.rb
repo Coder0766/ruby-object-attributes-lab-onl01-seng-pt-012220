6885a08438044da407deefb8e49f5521104b9e5b
Courtney DeRiggs <courtney.deriggs@gmail.com>
2020-02-06 07:52:48 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,25 @@
+# 1. Define ruby classes.
+# 2. Define methods that read from and write to instance variables.
+# 3. Create object properties using methods and instance variables.
+
+class Dog
+
+  #=> getter/reader
+  def name
+    @name
+  end
+
+  #=> setter/writer
+  def name=(name)
+    @name= name
+  end
+
+  def breed=(breed)
+    @breed= breed
+  end
+
+  def breed
+    @breed
+  end
+
+end
