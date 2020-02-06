33d403a665cf964d1981518ebddf25f055fe2979
Courtney DeRiggs <courtney.deriggs@gmail.com>
2020-02-06 07:27:20 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,15 +4,13 @@
 
 class Dog
 
-  attr_accessor :name
+  def initialize(name)
+    @name= name
+  end
 
-    def initialize(name)
-      @name= name
-    end
-
-    def name
-      @name
-    end
+  def name
+    @name
+  end
 
 end
 
